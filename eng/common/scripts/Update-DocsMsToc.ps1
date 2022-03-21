4f96501177ae12521a9977b180bd7e2cef72ef79
Daniel Jurek <djurek@microsoft.com>
2022-03-21 07:19:07 -0700

    Add support for service names containing a forward slash

--- eng/common/scripts/Update-DocsMsToc.ps1
+++ eng/common/scripts/Update-DocsMsToc.ps1
@@ -217,7 +217,7 @@ foreach ($service in $serviceNameList) {
     }
   }
 
-  $serviceReadmeBaseName = $service.ToLower().Replace(' ', '-')
+  $serviceReadmeBaseName = $service.ToLower().Replace(' ', '-').Replace('/', '-')
   $serviceTocEntry = [PSCustomObject]@{
     name            = $service;
     href            = "~/docs-ref-services/{moniker}/$serviceReadmeBaseName.md"
